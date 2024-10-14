500a58b36419b56728ecfc265a0fde4a558439a9
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-10-14 23:38:42 +0000

    Work on showRandomQuote function

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,16 +1,20 @@
 const quotes = [
-    { category: "Entertainment", text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae molestiae tenetur quos!"},
-    { category: "Sports", text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae molestiae tenetur quos!"},
-    { category: "Education", text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae molestiae tenetur quos!"}
+    { text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae molestiae tenetur quos!", category: "Entertainment" },
+    { text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae molestiae tenetur quos!", category: "Sports" },
+    { text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae molestiae tenetur quos!", category: "Education"}
 ];
 
+let quoteDisplay = document.getElementById('quoteDisplay');
 
 const showRandomQuote = () => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex];
+    quoteDisplay.innerHTML = `text: "${randomQuote.text}" - category: "${randomQuote.category}"`;
 }
 
 
 const addQuote = () => {
-    
+
 }
+
+quoteDisplay.addEventListener('click', showRandomQuote);
