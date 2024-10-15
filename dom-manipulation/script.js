eb14dc39a254aa839856bf9b881050ec4350e7b8
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-10-15 00:18:02 +0000

    Work on createAddQuoteForm  function

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,17 +4,17 @@ const quotes = [
     { text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae molestiae tenetur quos!", category: "Education"}
 ];
 
-let quoteDisplay = document.getElementById('quoteDisplay');
 
 const showRandomQuote = () => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex];
+    let quoteDisplay = document.getElementById('quoteDisplay');
     quoteDisplay.innerHTML = `<p>text: "${randomQuote.text}" - category: "${randomQuote.category}"</p>`;
 }
 
 
-const addQuote = () => {
-
+function createAddQuoteForm(){
+    
 }
 
 quoteDisplay.addEventListener('click', showRandomQuote);
