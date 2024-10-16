a24153d3d79cf881ab509344ce99c8468bbcaa7a
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-10-16 23:34:27 +0000

    Import and export json data

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -93,6 +93,17 @@ function loadQuotes() {
     URL.revokeObjectURL(url);
   }
 
+  function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
+
   document.getElementById('exportQuotes').addEventListener('click', exportQuotes);
 
 document.getElementById('newQuote').addEventListener('click', showRandomQuote);
